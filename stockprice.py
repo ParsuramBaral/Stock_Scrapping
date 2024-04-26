d112a606c09edaf0966b5f757b36b5d7db648b63
ParsuramBaral <parsurambaral42@gmail.com>
2024-04-26 21:56:10 +0545

    Update stockprice.py Version-3

--- stockprice.py
+++ stockprice.py
@@ -3,10 +3,12 @@ from bs4 import BeautifulSoup
 
 #For META
 company_list = ["META","TSLA","GOOGL","KO","AMZN","AAPL","NVDA","MSFT","NVR","BRK.A"]
-url_request = requests.get(url)
-html_request = BeautifulSoup(url_request.content, "html.parser")
-search_data = html_request.find("div", class_="QuoteStrip-lastPriceStripContainer")
-search_stock_price = search_data.find("span", class_="QuoteStrip-lastPrice")
-print("\nThe stock price for META is:", search_stock_price.text)
+for i in company_list:
+    url="https://www.cnbc.com/quotes/" + i
+    url_request = requests.get(url)
+    html_request = BeautifulSoup(url_request.content, "html.parser")
+    search_data = html_request.find("div", class_="QuoteStrip-lastPriceStripContainer")
+    search_stock_price = search_data.find("span", class_="QuoteStrip-lastPrice")
+    print("\nThe stock price for ",i ,"is", search_stock_price.text)
 
 
